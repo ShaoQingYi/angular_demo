2eab9474dd6eed6f17f87b0eaba0da676f3022d5
ChrisZX <amamiysola414@gmail.com>
2024-03-13 16:05:07 +0900

    「构建模板驱动表单」

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -9,18 +9,33 @@ import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+import { HeroformComponent } from './heroform/heroform.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HerosComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroformComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,// 为了组件能用 NgModule，需要引入这个
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+
+    // // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // // and returns simulated server responses.
+    // // Remove it when a real server is ready to receive requests.
+    // // "angular-in-memory-web-api": "^0.17.0",
+    // HttpClientInMemoryWebApiModule.forRoot(
+    //   InMemoryDataService, { dataEncapsulation: false }
+    // )
   ],
   providers: [],
   bootstrap: [AppComponent]
